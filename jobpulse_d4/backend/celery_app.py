b872798c9284e0e48919fd6610146f6bd7108f0e
Mihir Tale <tale@usc.edu>
2026-03-18 23:23:24 -0700

    Updated cron UTC time

--- jobpulse_d4/backend/celery_app.py
+++ jobpulse_d4/backend/celery_app.py
@@ -24,7 +24,7 @@ def make_celery() -> Celery:
             "daily-etl-ingest": {
             "task": "etl_coordinator",
             # "schedule": 300.0,           # (for testing)
-            "schedule": crontab(hour=22, minute=0),  
+            "schedule": crontab(hour=6, minute=30), #UTC Time zone  
         }
         }
     )

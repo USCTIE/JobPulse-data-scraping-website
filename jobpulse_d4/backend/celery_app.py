c85a546a4a0e29c8f00eb1947556806e31b1e6d0
Manas Dalvi <dalvimanas33@gmail.com>
2026-04-03 12:10:08 -0700

    adjust ETL ingestion time

--- jobpulse_d4/backend/celery_app.py
+++ jobpulse_d4/backend/celery_app.py
@@ -24,7 +24,7 @@ def make_celery() -> Celery:
             "daily-etl-ingest": {
             "task": "etl_coordinator",
             # "schedule": 300.0,           # (for testing)
-            "schedule": crontab(hour=6, minute=0), #UTC Time zone  
+            "schedule": crontab(hour=7, minute=15), #UTC Time zone  
         }
         }
     )
